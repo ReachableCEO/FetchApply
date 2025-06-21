8385dc1e4ddf6bf5092394d2ed9e59a89d9c0536
Charles N Wyble <reachableceo@turnsys.com>
2025-06-21 07:19:50 -0500

    some more enablement as i finalize OAM

--- newSrv.sh
+++ newSrv.sh
@@ -196,8 +196,18 @@ zsh \
 zsh-autocompletions \
 zsh-syntax-highlighting \
 fonts-powerline \
+webmin \
+usermin \
+cockpit \
 telnet \
 postfix 
+
+#Coming very soon, 2fa for webmin/cockpit/ssh
+#libpam-google-authenticator
+
+#https://www.ogselfhosting.com/index.php/2024/03/21/enabling-2fa-for-cockpit/
+#https://webmin.com/docs/modules/webmin-configuration/#two-factor-authentication
+#https://www.digitalocean.com/community/tutorials/how-to-set-up-multi-factor-authentication-for-ssh-on-ubuntu-18-04
 
 echo "Completed running $FUNCNAME"
 
