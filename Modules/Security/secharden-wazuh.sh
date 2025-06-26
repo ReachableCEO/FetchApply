396891af1cfee0a4102727d0b3b7e0f651522a46
Charles N Wyble <reachableceo@turnsys.com>
2025-06-26 16:41:42 -0500

    here goes a test...

--- Modules/Security/secharden-wazuh.sh
+++ Modules/Security/secharden-wazuh.sh
@@ -7,13 +7,21 @@ TSYS_NSM_CHECK="$(hostname |grep -c tsys-nsm ||true)"
 
 if [ "$TSYS_NSM_CHECK" -eq 0 ]; then
 
-curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
+if [ -f /usr/share/keyrings/wazuh.gpg ]; then
+    rm -f /usr/share/keyrings/wazuh.gpg
+fi
+
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import 
+chmod 644 /usr/share/keyrings/wazuh.gpg
 echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" > /etc/apt/sources.list.d/wazuh.list
 apt-get update
+
 WAZUH_MANAGER="tsys-nsm.knel.net" apt-get -y install wazuh-agent
+
 systemctl daemon-reload
 systemctl enable wazuh-agent
 systemctl start wazuh-agent
+
 echo "wazuh-agent hold" | dpkg --set-selections
 
 fi
