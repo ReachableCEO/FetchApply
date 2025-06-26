012fcb169810302d61157852ac4e7f792fe85d64
Charles N Wyble <reachableceo@turnsys.com>
2025-06-26 13:30:45 -0500

    all done with this todo now. finally.

--- Modules/Security/secharden-wazuh.sh
+++ Modules/Security/secharden-wazuh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
-echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" > etc/apt/sources.list.d/wazuh.list
+echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" > /etc/apt/sources.list.d/wazuh.list
 apt-get update
 WAZUH_MANAGER="tsys-nsm.knel.net" apt-get -y install wazuh-agent
 systemctl daemon-reload
