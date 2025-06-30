6e6a57f61bf88e3d8e6778a7d378400aa7b72787
ReachableCEO <charles@turnsys.com>
2025-06-30 13:28:13 -0500

    D.R.Y.

--- ProjectCode/Modules/Security/secharden-scap-stig.sh
+++ ProjectCode/Modules/Security/secharden-scap-stig.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-set -o functrace
-
-export PS4='(${BASH_SOURCE}:${LINENO}): - [${SHLVL},${BASH_SUBSHELL},$?] $ '
-
-function error_out()
-{
-        echo "Bailing out. See above for reason...."
-        exit 1
-}
-
-function handle_failure() {
-  local lineno=$1
-  local fn=$2
-  local exitstatus=$3
-  local msg=$4
-  local lineno_fns=${0% 0}
-  if [[ "$lineno_fns" != "-1" ]] ; then
-    lineno="${lineno} ${lineno_fns}"
-  fi
-  echo "${BASH_SOURCE[0]}: Function: ${fn} Line Number : [${lineno}] Failed with status ${exitstatus}: $msg"
-}
-
-trap 'handle_failure "${BASH_LINENO[*]}" "$LINENO" "${FUNCNAME[*]:-script}" "$?" "$BASH_COMMAND"' ERR
 
 function pi-detect()
 {
@@ -42,8 +16,6 @@ echo Completed running "$FUNCNAME"
 
 # Actual script logic starts here
 
-export DL_ROOT
-DL_ROOT="https://dl.knownelement.com/KNEL/FetchApply/"
 
 # Sourced from
 
