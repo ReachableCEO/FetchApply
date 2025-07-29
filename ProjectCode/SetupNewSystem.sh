870540840cc0860b1b846874041922a616b2f4b5
ReachableCEO <charles@turnsys.com>
2025-07-29 13:45:21 -0500

    use the tailscale installer

--- ProjectCode/SetupNewSystem.sh
+++ ProjectCode/SetupNewSystem.sh
@@ -98,8 +98,7 @@ function global-installPackages() {
 
   # Setup tailscale
 
-  curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-  curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+  curl -fsSL https://tailscale.com/install.sh | sh
 
   #
   #Patch the system
